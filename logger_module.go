be821236b0057d233044b07263d598be923938e0
LotiChang <smile9532023@gmail.com>
2024-05-31 17:27:17 +0800

    test1

---
+++ logger_module.go
@@ -0,0 +1,19 @@
+package loggermodule
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+)
+
+func GetLogger() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		logrus.Infof("[req][method => %s][path => %s]", c.Method(), c.Path())
+		if c.Method() == fiber.MethodPost {
+			logrus.Infof("[req][body => %s]", c.Body())
+		}
+
+		defer logrus.Infof("[res][status => %d][path => %s]", c.Response().StatusCode(), c.Path())
+
+		return c.Next()
+	}
+}
